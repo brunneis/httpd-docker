b1f1e57ead989fba6fda168622c9c3052af71a8b
Rodrigo <dev@brunneis.com>
2019-12-28 11:54:43 +0100

    httpd 2.4.41, minor changes

--- 2.4/build-push.sh
+++ 2.4/build-push.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-docker build -t brunneis/httpd-centos .
-docker build -t brunneis/httpd-centos:2.4 .
-docker push brunneis/httpd-centos
+source ../env.sh
+docker build --build-arg HTTPD_VERSION=$HTTPD_VERSION -t brunneis/httpd .
+
+docker tag brunneis/httpd brunneis/httpd:2.4
+docker tag brunneis/httpd brunneis/httpd:$(echo $HTTPD_VERSION | cut -c1-3)
+
+docker push brunneis/httpd:latest
+docker push brunneis/httpd:$HTTPD_VERSION
+docker push brunneis/httpd:$(echo $HTTPD_VERSION | cut -c1-3)
---
+++ env.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+HTTPD_VERSION=2.4.41
